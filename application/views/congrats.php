65894994529335a4340b262f88301d0c1c9cf7fe
Balaji <balaji.sp@digitalchakra.in>
2013-07-19 21:19:44 +0530

    added styling for congrats page

--- application/views/congrats.php
+++ application/views/congrats.php
@@ -1,22 +1,34 @@
-<p> .</p>
 
-<div class="row-fluid">
-	<div class="offset1 span2 ads final-ads">
+<style type="text/css">
+.s_heading{
+	width: 96%;
+	height: 60px;
+	background: #f07057;
+	line-height: 60px;
+	color: #fff;
+	font-size: 1.6em;
+	padding-left: 50px;
+}
 
-	</div>
-		
-	<div class="span6">
-		<div class="download-main">
+	.clickr{
+	width: 200px; height: 40px; line-height: 40px;
+	 display: block; text-align: center;
+	 margin-top: 100px;
+	cursor: pointer; text-decoration: none;
+	background: #f07057 url(../img/arrow_btn.png) no-repeat right center;	border: solid 1px #c5533d;	color: #fff;	
+}
+
+.clickr:hover{
+	background-color: #da5e46;
+	border: solid 1px #d05840;
+}
+
+</style>
+
+		<div class="s_heading">
 			Acount activated successfully
-			<div class="download-main-inner">
-
-					<a class="dbutton" href="<? echo base_url('login');?>/">Login</a>
-			</div> 
-			
 		</div>
-		
-	</div>
-	<div class="span2 ads final-ads">
+		<div class="download-main-inner">
+			<a class="dbutton clickr" href="<? echo base_url('login');?>/">Login</a>
+		</div> 
 
-	</div>
-</div>

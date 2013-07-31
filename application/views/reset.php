a30c950cb19a5cc636e19fd4ed2a2adaa54bd288
Balaji <balaji.sp@digitalchakra.in>
2013-07-31 16:39:21 +0530

    Added design for Reset and Reset_success pages

--- application/views/reset.php
+++ application/views/reset.php
@@ -1,31 +1,24 @@
-<div class="row-fluid">
-	<div class="offset1 span2 ads final-ads">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/sss_resume.css"); ?>" />
+<div class="form_title">
+	<h3>Enter new password</h3>
+</div>
 
-	</div>
-		
-	<div class="span6">
+	
+<div class="left_form_feedback">
+<div class="container">
 		<div class="download-main">
-			Enter new password
 			<div class="download-main-inner">
 				<span style="color:#ff0000; font-size:12px;"><?php echo $error; ?></span>
-				<form class="form-inline" action="<?php echo base_url('forget/activation'); ?>" method="POST">
-  					 <input type="password" class="input-medium" placeholder="Password" id="passowrd" name="password">
-  					 <input type="password" class="input-medium" placeholder="Confirm Password" id="cpassowrd" name="cpassword">
+				<form action="<?php echo base_url('forget/activation'); ?>" method="POST">
+  					 <input type="password" placeholder="Password" id="passowrd" name="password"><br/>
+  					 <input type="password" placeholder="Confirm Password" id="cpassowrd" name="cpassword"><br/>
   					 <input type="hidden" value="<?php echo $forget; ?>" name="acode">
   					 <input type="hidden" value="<?php echo $id; ?>" name="uid">
-				     <input type="submit" id="restsubmit" class="btn btn-small btn-primary" />
+				     <input type="submit" id="restsubmit" class="fb_btn" style="cursor:pointer;" />
 				</form>
 			</div> 
 			
 		</div>
 		
 	</div>
-	<div class="span2 ads final-ads">
-
-	</div>
 </div>
-<script type="text/javascript">
-$(document).ready(function(){
-	$('.signIn').hide();
-});
-</script>
--- application/views/reset_success.php
+++ application/views/reset_success.php
@@ -1,20 +1,38 @@
-<div class="row-fluid">
-	<div class="offset1 span2 ads final-ads">
+<style type="text/css">
+.s_heading{
+	width: 96%;
+	height: 60px;
+	background: #f07057;
+	line-height: 60px;
+	color: #fff;
+	font-size: 1.6em;
+	padding-left: 50px;
 
-	</div>
-		
-	<div class="span6">
-		<div class="download-main">
+}
+
+	.clickr{
+
+	width: 200px; height: 40px; line-height: 40px;
+	 display: block; text-align: center;
+	 margin: 0 auto;	cursor: pointer; text-decoration: none;
+	background: #f07057;	border: solid 1px #c5533d;	color: #fff;	
+	margin-top:20px;
+}
+
+
+
+
+.clickr:hover{
+	background-color: #da5e46;
+	border: solid 1px #d05840;
+}
+.download_logox{background: transparent url(../assets/img/download_logo.png) center no-repeat; text-align:center; height:280px; margin-top: 50px; padding:0px;  display:block;}
+</style>
+
+		<div class="s_heading">
 			Your password has been changed successfully !
-			<div class="download-main-inner">
-
-					<a class="dbutton" href="<? echo base_url('login');?>/">Login</a>
-			</div> 
-			
 		</div>
+			<div class="download_logox">
+			</div>
+			<a class="dbutton clickr" href="<? echo base_url('login');?>/">Login</a>
 		
-	</div>
-	<div class="span2 ads final-ads">
-
-	</div>
-</div>

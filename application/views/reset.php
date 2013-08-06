39dbffac1671b5cec42dabe580da8662a3ed0252
dc-mani <manikandan@digitalchakra.in>
2013-08-06 13:09:48 +0530

    issue #185

--- application/views/reset.php
+++ application/views/reset.php
@@ -6,29 +6,29 @@
 
 	
 <div class="left_form_feedback">
-	<span>In order to verify your password, please enter it in the field below and re-enter it in the next field. A good password should contain capital and lower case letters, numbers as well as symbols</span><br><br>
+	<span>In order to verify your password, please enter it in the field below and re-enter it in the next field. A good password should contain capital and lower case letters, numbers as well as symbols.</span><br><br>
 <div class="container">
 		<div class="download-main">
 			<div class="download-main-inner">
-				<span style="color:#ff0000; font-size:14px;"><?php echo $error; ?></span>
+				<span style="color:#ff0000; font-size:14px; position:absolute; margin:22px 0 0 184px;"><?php echo $error; ?></span>
 				<br>
 				<br>
 				<form action="<?php echo base_url('forget/activation'); ?>" method="POST">
         <table>
-        	<tr>
+        	<tr style="border:none;">
        		 <td>
            	<label class="pr_left1">Enter New Password: </label>
            </td>
            <td>
-            <input type="password" placeholder="Password" id="passowrd" name="password">
+            <input style="margin-left:20px;" type="password" placeholder="Password" id="passowrd" name="password">
            </td>
          </tr>
-         <tr>
+         <tr style="border:none;">
          <td>
          	<label class="pr_left1">Re-enter New Password: </label>
          </td>
          <td>
-         	<input type="password" placeholder="Confirm Password" id="cpassowrd" name="cpassword">
+         	<input style="margin-left:20px;" type="password" placeholder="Confirm Password" id="cpassowrd" name="cpassword">
          </td>
         </tr>
        </table>

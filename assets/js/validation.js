b4da1b5d6757550fe8ef71de69884c94d3b124c3
dc-mani <manikandan@digitalchakra.in>
2013-08-07 11:53:24 +0530

    issue #181

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -53,7 +53,7 @@ function validate(title,id,mandatory,max_length,min_length,type,display)
 		var format = /^[a-zA-Z ]+$/;
 		if(!data.match(format))
 		{
-			var msg=title+' can contain alphapets and empty space only.';
+			var msg=title+' can contain letters and empty space only.';
 			if(!display)
 				alert(msg);
 			else
